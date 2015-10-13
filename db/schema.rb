2ff0d225a8415f84d13b5dd552f95c1cb5fba706
Fred Duby <fcd1@columbia.edu>
2015-10-13 11:28:09 -0400

    Add migrations for csv import tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012202657) do
+ActiveRecord::Schema.define(version: 20151013152356) do
+
+  create_table "digital_object_imports", force: :cascade do |t|
+    t.text     "digital_object_data", limit: 65535
+    t.integer  "status",              limit: 4,     null: false
+    t.integer  "import_job_id",       limit: 4,     null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
+  end
+
+  add_index "digital_object_imports", ["import_job_id"], name: "index_digital_object_imports_on_import_job_id", using: :btree
+
+  create_table "import_jobs", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
+    t.integer  "user_id",    limit: 4,   null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "import_jobs", ["user_id"], name: "index_import_jobs_on_user_id", using: :btree
 
   create_table "records", force: :cascade do |t|
     t.string   "test_string", limit: 255
@@ -19,4 +38,10 @@ ActiveRecord::Schema.define(version: 20151012202657) do
     t.datetime "updated_at",              null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", limit: 255, null: false
+  end
+
+  add_foreign_key "digital_object_imports", "import_jobs"
+  add_foreign_key "import_jobs", "users"
 end
